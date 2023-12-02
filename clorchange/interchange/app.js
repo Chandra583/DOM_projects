41adbff1419a0108db5376546873ab7e3e94224e
Chandra583 <chandrashekhargawda2000@gmail.com>
2023-12-02 23:17:39 +0530

    s

--- clorchange/interchange/app.js
+++ clorchange/interchange/app.js
@@ -8,37 +8,37 @@ btn.forEach(function (btns) {
   btns.addEventListener("click", function (e) {
     console.log(e.target.id);
 
-    // if (e.target.id === "purple") {
-    //   body.style.backgroundColor = e.target.id;
-    //   result.innerText = `you clicked on ${e.target.id}`;
-    //   result.classList.remove("bl");
-    // }
-    // if (e.target.id === "Green") {
-    //   body.style.backgroundColor = e.target.id;
-    //   result.innerText = `you clicked on ${e.target.id}`;
-    //   result.classList.remove("bl");
-    // }
-    // if (e.target.id === "white") {
-    //   body.style.backgroundColor = e.target.id;
-    //   result.innerText = `you clicked on ${e.target.id}`;
-    //   result.classList.remove("bl");
-    // }
-    // if (e.target.id === "yellow") {
-    //   body.style.backgroundColor = e.target.id;
-    //   result.innerText = `you clicked on ${e.target.id}`;
-    //   result.classList.remove("bl");
-    // }
+    if (e.target.id === "purple") {
+      body.style.backgroundColor = e.target.id;
+      result.innerText = `you clicked on ${e.target.id}`;
+      result.classList.remove("bl");
+    }
+    if (e.target.id === "Green") {
+      body.style.backgroundColor = e.target.id;
+      result.innerText = `you clicked on ${e.target.id}`;
+      result.classList.remove("bl");
+    }
+    if (e.target.id === "white") {
+      body.style.backgroundColor = e.target.id;
+      result.innerText = `you clicked on ${e.target.id}`;
+      result.classList.remove("bl");
+    }
+    if (e.target.id === "yellow") {
+      body.style.backgroundColor = e.target.id;
+      result.innerText = `you clicked on ${e.target.id}`;
+      result.classList.remove("bl");
+    }
+    if (e.target.id === "blue") {
+      body.style.backgroundColor = e.target.id;
+      result.innerText = `you clicked on ${e.target.id}`;
+      //how to change black color tect into white in js
+      //change into white color
+      result.classList.add("bl");
+    }
     // if (e.target.id === "blue") {
-    //   body.style.backgroundColor = e.target.id;
-    //   result.innerText = `you clicked on ${e.target.id}`;
-    //   //how to change black color tect into white in js
-    //   //change into white color
     //   result.classList.add("bl");
     // }
-    // // if (e.target.id === "blue") {
-    // //   result.classList.add("bl");
-    // // }
-    //write siwtch case
+    // write siwtch case
     switch (e.target.id) {
       case "purple":
         body.style.backgroundColor = e.target.id;

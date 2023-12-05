797722cef924db8069776c68e5a544b4dad2bdce
Chandra583 <chandrashekhargawda2000@gmail.com>
2023-12-05 20:05:11 +0530

    n

--- clorchange/interchange/app.js
+++ clorchange/interchange/app.js
@@ -2,39 +2,39 @@ const btn = document.querySelectorAll(".button");
 const result = document.querySelector(".result");
 const body = document.body;
 
-btn.forEach(function (btns) {
-  //
+// btn.forEach(function (btns) {
+//   //
 
-  btns.addEventListener("click", function (e) {
-    console.log(e.target.id);
+//   btns.addEventListener("click", function (e) {
+//     console.log(e.target.id);
 
-    if (e.target.id === "purple") {
-      body.style.backgroundColor = e.target.id;
-      result.innerText = `you clicked on ${e.target.id}`;
-      result.classList.remove("bl");
-    }
-    if (e.target.id === "Green") {
-      body.style.backgroundColor = e.target.id;
-      result.innerText = `you clicked on ${e.target.id}`;
-      result.classList.remove("bl");
-    }
-    if (e.target.id === "white") {
-      body.style.backgroundColor = e.target.id;
-      result.innerText = `you clicked on ${e.target.id}`;
-      result.classList.remove("bl");
-    }
-    if (e.target.id === "yellow") {
-      body.style.backgroundColor = e.target.id;
-      result.innerText = `you clicked on ${e.target.id}`;
-      result.classList.remove("bl");
-    }
-    if (e.target.id === "blue") {
-      body.style.backgroundColor = e.target.id;
-      result.innerText = `you clicked on ${e.target.id}`;
-      //how to change black color tect into white in js
-      //change into white color
-      result.classList.add("bl");
-    }
+//     if (e.target.id === "purple") {
+//       body.style.backgroundColor = e.target.id;
+//       result.innerText = `you clicked on ${e.target.id}`;
+//       result.classList.remove("bl");
+//     }
+//     if (e.target.id === "Green") {
+//       body.style.backgroundColor = e.target.id;
+//       result.innerText = `you clicked on ${e.target.id}`;
+//       result.classList.remove("bl");
+//     }
+//     if (e.target.id === "white") {
+//       body.style.backgroundColor = e.target.id;
+//       result.innerText = `you clicked on ${e.target.id}`;
+//       result.classList.remove("bl");
+//     }
+//     if (e.target.id === "yellow") {
+//       body.style.backgroundColor = e.target.id;
+//       result.innerText = `you clicked on ${e.target.id}`;
+//       result.classList.remove("bl");
+//     }
+//     if (e.target.id === "blue") {
+//       body.style.backgroundColor = e.target.id;
+//       result.innerText = `you clicked on ${e.target.id}`;
+//       //how to change black color tect into white in js
+//       //change into white color
+//       result.classList.add("bl");
+//     }
     // if (e.target.id === "blue") {
     //   result.classList.add("bl");
     // }
